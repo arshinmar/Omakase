aa6c1ec54e6b0790d23b62ecc964eac517d82dc9
arshinmar <arsh.kadakia18@gmail.com>
2020-08-23 01:39:51 -0400

    temp change

--- wsgi.py
+++ wsgi.py
@@ -10,7 +10,17 @@ def dynamic_page():
     return '<h1>Hello World</h1>'
 '''
 
-from app.main import app
+from flask import Flask
+from function_files.true_recognition import main_pipeline
+
+app = Flask(__name__)
+
+@app.route("/")
+def home_view():
+    image_path='images/screenshot.png'
+    full_list=main_pipeline(image_path)
+    output_val={'output':full_list}
+    return output_val
 
 if __name__ == '__main__':
     app.run()   #host='0.0.0.0', port='8000', debug=True)
