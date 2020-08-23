25c772d18d87e24ee2dfc633650be2b31375641e
arshinmar <arsh.kadakia18@gmail.com>
2020-08-23 01:35:33 -0400

    add image recognition

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from function_files.true_recognition import main_pipeline
 
 app = Flask(__name__)
 
 @app.route("/")
 def home_view():
-    return '<h1>Test Hello World</h1>'
+    image_path='images/screenshot.png'
+    full_list=main_pipeline(image_path)
+    output_val={'output':full_list}
+    return output_val
