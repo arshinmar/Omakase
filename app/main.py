58abd1dcdf0b4e0cf3bfb6219fc967751168f389
arshinmar <arsh.kadakia18@gmail.com>
2020-08-23 02:07:24 -0400

    remove secret key

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from function_files.true_recognition import main_pipeline
 
 app = Flask(__name__)
 
@@ -9,3 +8,10 @@ def home_view():
     full_list=main_pipeline(image_path)
     output_val={'output':full_list}
     return output_val
+
+    '''
+    raw_recipes_file = "RAW_recipes.pkl"
+    pp_recipes_file = "PP_recipes.pkl"
+    ingr_map_file = 'ingr_map.pkl'
+    data_obj = recipe_data_class(raw_recipes_file, pp_recipes_file, ingr_map_file)
+    '''
