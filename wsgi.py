3afd67cd266a9cd16cce45d9390ebbab697e2586
arshinmar <arsh.kadakia18@gmail.com>
2020-08-23 00:08:14 -0400

    wsgi/main changes

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from flask import Flask
+'''from flask import Flask
 from flask_restful import Api, Resource
 
 app = Flask(__name__)
@@ -8,6 +8,9 @@ app = Api(app)
 def dynamic_page():
     temp_dict={'1':'2'}
     return '<h1>Hello World</h1>'
+'''
+
+from app.main import app
 
 if __name__ == '__main__':
     app.run()   #host='0.0.0.0', port='8000', debug=True)
